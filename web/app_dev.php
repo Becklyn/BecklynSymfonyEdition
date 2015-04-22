45189c44bbf331f82896a853b1edecf31e7d7625
Jannik Zschiesche <jz@becklyn.com>
2015-04-22 17:33:09 +0200

    Allow more local IPs for app_dev.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -3,18 +3,33 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
-// If you don't want to setup permissions the proper way, just uncomment the following PHP line
-// read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
-//umask(0000);
-
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
+$isLocalIp = function ($ip)
+{
+    if (!$ip)
+    {
+        return false;
+    }
+
+    if (in_array($ip, ['127.0.0.1', 'fe80::1', '::1']))
+    {
+        return true;
+    }
+
+    // allowed (= local) IPs include
+    // 10.0.0.0 – 10.255.255.255
+    // 172.16.0.0 – 172.31.255.255
+    // 192.168.0.0 – 192.168.255.255
+    return 1 === preg_match("/^(10\\.|192\\.168\\.|172\\.(1[6-9]|2[0-9]|3[0-1])\\.)/", $ip);
+};
+
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1')) || php_sapi_name() === 'cli-server')
+    || !(php_sapi_name() === 'cli-server' || $isLocalIp($_SERVER['REMOTE_ADDR']))
 ) {
     header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+    exit("Forbidden.");
 }
 
 $loader = require_once __DIR__.'/../var/bootstrap.php.cache';
