272e0d57640aa752f80e8e600a0c7274be98a9c3
Jannik Zschiesche <jz@becklyn.com>
2015-04-22 11:00:29 +0200

    Group bundles in AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -8,6 +8,7 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = array(
+            // region Core bundles
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
@@ -15,7 +16,11 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            // endregion
+
+            // region Application bundles
             new AppBundle\AppBundle(),
+            // endregion
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
