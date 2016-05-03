85cf4b4840dd46510f3bd35b67edc4a5a02a0844
Jannik Zschiesche <jz@becklyn.com>
2016-05-03 16:24:04 +0200

    Update to Symfony 3.0.5

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -3,11 +3,12 @@
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
+
 class AppKernel extends Kernel
 {
-    public function registerBundles()
+    public function registerBundles ()
     {
-        $bundles = array(
+        $bundles = [
             // region Core bundles
             new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
@@ -25,9 +26,10 @@ class AppKernel extends Kernel
             // region Application bundles
             new \AppBundle\AppBundle(),
             // endregion
-        );
+        ];
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        if (in_array($this->getEnvironment(), ['dev', 'test'], true))
+        {
             $bundles[] = new \Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new \Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new \Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
@@ -39,10 +41,40 @@ class AppKernel extends Kernel
 
 
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
-    public function registerContainerConfiguration(LoaderInterface $loader)
+    public function getRootDir ()
     {
-        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+        return __DIR__;
+    }
+
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCacheDir ()
+    {
+        return dirname(__DIR__) . '/var/cache/' . $this->getEnvironment();
+    }
+
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getLogDir ()
+    {
+        return dirname(__DIR__) . '/var/logs';
+    }
+
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function registerContainerConfiguration (LoaderInterface $loader)
+    {
+        $loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
     }
 }
--- web/app_dev.php
+++ web/app_dev.php
@@ -3,6 +3,11 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
+/**
+ * @var Composer\Autoload\ClassLoader $loader
+ */
+$loader = require __DIR__.'/../app/autoload.php';
+
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 $localAccess = new \BecklynSymfonyBase\LocalAccess($_SERVER);
@@ -11,8 +16,6 @@ if (!$localAccess->allowLocalAccess())
     header('HTTP/1.0 403 Forbidden');
     exit("Forbidden.");
 }
-
-$loader = require_once __DIR__.'/../var/bootstrap.php.cache';
 Debug::enable();
 
 $kernel = new AppKernel('dev', true);
